3109425663e72868f64d2caa02d0cf233acc4385
WenJieChew <wenjie.chew@gmail.com>
2016-06-09 15:50:53 +0800

    Project

--- app/src/main/java/com/example/wenjiechew/project/MainActivity.java
+++ app/src/main/java/com/example/wenjiechew/project/MainActivity.java
@@ -1,22 +1,36 @@
 package com.example.wenjiechew.project;
 
+import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.app.FragmentTransaction;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     private String TAG = "ListView";
 
+    private int year, month, day;
+    private static Button date;
+
+    private String listName;
+
     private DBAccess dbAccess;
 
-    Dialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +39,70 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        dbAccess = DBAccess.getInstance(this);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.d(TAG,"add fab button");
 
-                dialog = new Dialog(MainActivity.this);
-                dialog.setTitle("New Itinerary List");
+                //Inflating custom layout
+                LayoutInflater inflater = (LayoutInflater) getLayoutInflater();
+                View customView = inflater.inflate(R.layout.custom_additinenarylist_layout, null);
+
+                //Define datepicker
+                //final DatePicker datePicker = (DatePicker) customView.findViewById(R.id.datePicker);
+                final EditText listEditText = (EditText) customView.findViewById(R.id.listEditText);
+                final EditText datePickerText = (EditText) customView.findViewById(R.id.txtdate);
+
+               datePickerText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                   @Override
+                   public void onFocusChange(View v, boolean hasFocus) {
+                       if(hasFocus){
+                           DateDialog dialog = new DateDialog(v);
+                           FragmentTransaction ft = getFragmentManager().beginTransaction();
+                           dialog.show(ft,"DatePicker");
+                       }
+                   }
+               });
+
+
+
+                //Build Dialog
+                AlertDialog.Builder buider = new AlertDialog.Builder(MainActivity.this);
+                buider.setView(customView);
+                buider.setTitle("New Itinerary List");
+                buider.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Places places = new Places();
+                        places.set_placeName(listEditText.getText().toString());
+
+                        //// TODO: 9/6/2016 TO DO THE DATE TO UPLOAD TO DB
+//                        year = datePicker.getYear();
+//                        month = datePicker.getMonth();
+//                        day = datePicker.getDayOfMonth();
+
+
+                        dbAccess.openWrite();
+                        dbAccess.Insert(dbAccess.TABLENAME_PLACE, places);
+                        dbAccess.close();
+
+
+                    }
+
+                });
+                buider.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                buider.create().show();
+
+
 
 
             }
@@ -60,4 +130,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
